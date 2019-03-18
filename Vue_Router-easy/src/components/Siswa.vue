aeaf9bbf1b6bdee5f57b4dd8f03eec857d8d9ba8
Addict <47271105+Addict@users.noreply.github.com>
2019-03-18 15:55:02 +0700

    Add files via upload

---
+++ Vue_Router-easy/src/components/Siswa.vue
@@ -0,0 +1,29 @@
+<template>
+ <div><br>
+  <center>
+  <table border="1">
+    <tr>
+      <td><center><b>Kelas</b></center></td>
+      <td><center><b>Nama</b></center></td>
+    </tr>
+    <tr>
+      <td>XII - RPL 1</td>
+      <td>Aditia Dwi Saputra</td>
+    </tr>
+    <tr>
+      <td>XII - RPL 2</td>
+      <td>M. Faisal Obara</td>
+    </tr>
+    <tr>
+      <td>XII - RPL 3</td>
+      <td>Rangga Jatnika</td>
+    </tr>
+  </table>
+  </center>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
